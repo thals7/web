c43cfe1315a0146c8f47b5bb6769c32b9264f99f
thals7 <thals_7@naver.com>
2020-10-04 22:49:05 +0900

    WEB2-Python ~8

---
+++ WEB2_Python/index.py
@@ -0,0 +1,22 @@
+#!python
+print("content-type: text/html; charset=utf-8\n")
+import cgi
+form = cgi.FieldStorage()
+pageId = form["id"].value
+print('''<!doctype html>
+<html>
+<head>
+  <title>WEB1 - Welcome</title>
+  <meta charset="utf-8">
+</head>
+<body>
+  <h1><a href="index.py">WEB</a></h1>
+  <ol>
+    <li><a href="index.py?id=HTML">HTML</a></li>
+    <li><a href="index.py?id=CSS">CSS</a></li>
+    <li><a href="index.py?id=JavaScript">JavaScript</a></li>
+  </ol>
+  <h2>{title}</h2>
+  <p></p>
+</body>
+</html>'''.format(title=pageId))
